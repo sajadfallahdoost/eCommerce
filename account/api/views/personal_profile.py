799cc0c9544392350459bdcb67b3d0e577c43e4d
sajad <fallahdoostsajad@gmail.com>
2024-07-03 18:33:06 +0330

    refactor(account API): enhance swaager doc

--- account/api/views/personal_profile.py
+++ account/api/views/personal_profile.py
@@ -4,8 +4,71 @@ from rest_framework import status
 from account.models import PersonalProfile
 from account.api.serializers import PersonalProfileSerializer
 from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+# Example for request body
+personal_profile_example = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'first_name': openapi.Schema(type=openapi.TYPE_STRING, description='First Name'),
+        'last_name': openapi.Schema(type=openapi.TYPE_STRING, description='Last Name'),
+        'national_code': openapi.Schema(type=openapi.TYPE_STRING, description='National Code'),
+        'gender': openapi.Schema(type=openapi.TYPE_STRING, enum=['male', 'female'], description='Gender'),
+        'phone': openapi.Schema(type=openapi.TYPE_INTEGER, description='Phone'),
+        'birth_date': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATE, description='Birth Date'),
+        'job': openapi.Schema(type=openapi.TYPE_STRING, description='Job')
+    },
+    required=['first_name', 'last_name', 'national_code', 'gender', 'phone'],
+    example={
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'national_code': '1234567890',
+        'gender': 'male',
+        'phone': 1234567890,
+        'birth_date': '2000-01-01',
+        'job': 'Developer'
+    }
+)
+
+# Example for response body
+personal_profile_response_example = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='ID of the personal profile'),
+        'first_name': openapi.Schema(type=openapi.TYPE_STRING, description='First Name'),
+        'last_name': openapi.Schema(type=openapi.TYPE_STRING, description='Last Name'),
+        'national_code': openapi.Schema(type=openapi.TYPE_STRING, description='National Code'),
+        'gender': openapi.Schema(type=openapi.TYPE_STRING, enum=['male', 'female'], description='Gender'),
+        'phone': openapi.Schema(type=openapi.TYPE_INTEGER, description='Phone'),
+        'birth_date': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATE, description='Birth Date'),
+        'job': openapi.Schema(type=openapi.TYPE_STRING, description='Job')
+    },
+    example={
+        'id': 1,
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'national_code': '1234567890',
+        'gender': 'male',
+        'phone': 1234567890,
+        'birth_date': '2000-01-01',
+        'job': 'Developer'
+    }
+)
 
 
+@swagger_auto_schema(
+    method='get',
+    responses={200: openapi.Response('List of personal profiles', PersonalProfileSerializer(many=True), examples={'application/json': [personal_profile_response_example.example]})}
+)
+@swagger_auto_schema(
+    method='post',
+    request_body=personal_profile_example,
+    responses={
+        201: openapi.Response('Personal profile created', PersonalProfileSerializer, examples={'application/json': personal_profile_response_example.example}),
+        400: 'Bad Request - Invalid data'
+    }
+)
 @api_view(['GET', 'POST'])
 def personal_profile_list_create(request):
     """
@@ -24,6 +87,29 @@ def personal_profile_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@swagger_auto_schema(
+    method='get',
+    responses={
+        200: openapi.Response('Personal profile details', PersonalProfileSerializer, examples={'application/json': personal_profile_response_example.example}),
+        404: 'Not Found'
+    }
+)
+@swagger_auto_schema(
+    method='put',
+    request_body=personal_profile_example,
+    responses={
+        200: openapi.Response('Personal profile updated', PersonalProfileSerializer, examples={'application/json': personal_profile_response_example.example}),
+        400: 'Bad Request - Invalid data',
+        404: 'Not Found'
+    }
+)
+@swagger_auto_schema(
+    method='delete',
+    responses={
+        204: 'No Content',
+        404: 'Not Found'
+    }
+)
 @api_view(['GET', 'PUT', 'DELETE'])
 def personal_profile_detail(request, pk):
     """
