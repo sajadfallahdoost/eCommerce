e1d44265db4501867353c46ad8b757288d9c859a
sajad <fallahdoostsajad@gmal.com>
2024-07-27 12:09:08 +0330

    test(config test case)

---
+++ shop/api/tests/test_shop.py
@@ -0,0 +1,49 @@
+import requests
+from django.test import TestCase
+
+
+class CartAPITestCase(TestCase):
+    def test_get_all_carts(self):
+        response = requests.get('http://78.157.51.34:8000/api/carts/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
+    def test_create_cart(self):
+        payload = {'user': 1}
+        response = requests.post('http://78.157.51.34:8000/api/carts/', json=payload)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('id', response.json())
+        self.assertEqual(response.json()['user'], 1)
+
+    def test_get_cart_details(self):
+        response = requests.get('http://78.157.51.34:8000/api/carts/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['id'], 1)
+
+    def test_update_cart(self):
+        payload = {'user': 2}
+        response = requests.put('http://78.157.51.34:8000/api/carts/1/', json=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['user'], 2)
+
+    def test_delete_cart(self):
+        response = requests.delete('http://78.157.51.34:8000/api/carts/1/')
+        self.assertEqual(response.status_code, 204)
+
+    def test_create_cart_invalid_data(self):
+        payload = {'user': 'invalid'}
+        response = requests.post('http://78.157.51.34:8000/api/carts/', json=payload)
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_cart_non_existing(self):
+        response = requests.get('http://78.157.51.34:8000/api/carts/999/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_cart_invalid_data(self):
+        payload = {'user': 'invalid'}
+        response = requests.put('http://78.157.51.34:8000/api/carts/1/', json=payload)
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_cart_non_existing(self):
+        response = requests.delete('http://78.157.51.34:8000/api/carts/999/')
+        self.assertEqual(response.status_code, 404)
